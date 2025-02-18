b7e17ec4aba817b97e3c2d22e30b2bdc705ad99a
ShenanVindinu <shenanvindinu@gmail.com>
2025-02-18 10:00:24 +0530

    eureka configed

--- eureka-server/src/main/java/lk/ijse/gdse/shenan/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/lk/ijse/gdse/shenan/eurekaserver/EurekaServerApplication.java
@@ -2,9 +2,10 @@ package lk.ijse.gdse.shenan.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEruekaServer
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
