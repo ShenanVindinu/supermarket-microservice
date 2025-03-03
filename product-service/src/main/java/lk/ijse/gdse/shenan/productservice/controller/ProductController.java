2484da6b7c434d16fac4348fc90c61ae59cf9f09
ShenanVindinu <shenanvindinu@gmail.com>
2025-03-03 09:21:48 +0530

    product service get all added

--- product-service/src/main/java/lk/ijse/gdse/shenan/productservice/controller/ProductController.java
+++ product-service/src/main/java/lk/ijse/gdse/shenan/productservice/controller/ProductController.java
@@ -1,11 +1,13 @@
 package lk.ijse.gdse.shenan.productservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @GetMapping("all")
     public String getProducts() {
         return "Product Service: Product1, Product2, Product3";
     }
