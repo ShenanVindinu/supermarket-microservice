024b6a16a74c2185cb5bf7fd74477f4e2dd27dc7
ShenanVindinu <shenanvindinu@gmail.com>
2025-03-03 09:51:46 +0530

    Customer_Service python script implemented

--- customer-service/customer_service.py
+++ customer-service/customer_service.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify
+from py_eureka_client import eureka_client
 
 app = Flask(__name__)
 
-CONTEXT_PATH = "/customer"
+CONTEXT_PATH = "/customer-service"
 SERVICE_PORT = 5000
+EUREKA_SERVER = "http://localhost:8761/eureka/"
+
+eureka_client.init(
+    eureka_server= EUREKA_SERVER,
+    app_name= "CUSTOMER-SERVICE",
+    instance_port= SERVICE_PORT
+)
 
 
-app.run(port=SERVICE_PORT)
-
-@app.route(f'{CONTEXT_PATH}/customer', methods=["GET"])
+# http://127.0.0.1:5000/customer-service/customers
+@app.route(f'{CONTEXT_PATH}/customers', methods=["GET"])
 def get_customer():
     customer_list=[
         {"id":1, "name":"John Doe", "email":"jhon@gmail.com"},
